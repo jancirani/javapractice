b8370b1f0c7206a87c671f210b95bc233577acf4
janci <jancilondon@gmail.com>
2017-08-26 19:40:37 +0100

    reg tests for password

--- Shopping/src/main/java/com/asos/Registration.java
+++ Shopping/src/main/java/com/asos/Registration.java
@@ -49,4 +49,9 @@ public class Registration extends DriverManager {
        return driver.findElement(By.cssSelector("#Email-error")).getText();
 
    }
+   public String passwordverification()
+   {
+      return driver.findElement(By.cssSelector("#Password-error")) .getText();
+   }
+
 }
